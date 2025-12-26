eda53577a19ef545529b95955654d1b3c8832f7b
GitHub Action <action@github.com>
2025-12-26 23:43:40 +0000

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="simple-file-transfer",
-    version="0.1.1",
+    version="0.2.1",
     packages=find_packages(),
     install_requires=[
         "flask>=2.3.0",
